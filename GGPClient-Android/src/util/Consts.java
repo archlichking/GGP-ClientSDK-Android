12c0ea9c981fa9c03d054c1462a6c65daf5065ca
Vincent Xie [Xie WeiShen] <vincent.xie@gree.co.jp>
2012-03-28 18:28:54 +0800

    Add GGPClient-Android folder

---
+++ GGPClient-Android/src/util/Consts.java
@@ -0,0 +1,19 @@
+
+package util;
+
+public class Consts {
+    public static final String READY = "READY";
+
+    public static final String FAILED = "FAILED";
+
+    public static final String UNKNOWN = "UNKNOWN";
+
+    public static final String SUCCESS = "SUCCESS";
+    
+    public static final int startIndex_0 = 0;
+    
+    public static final int startIndex_1 = 1;
+
+    public static final int pageSize = 30;
+
+}
