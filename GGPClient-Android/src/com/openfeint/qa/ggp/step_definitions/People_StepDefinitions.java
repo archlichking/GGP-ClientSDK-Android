01bd39d80b69a18d18a3f61a5d324b9e398059a4
Lei Zhu <thunderzhulei@gmail.com>
2012-04-30 14:49:01 -0700

    1. async steps and sample in People_StepDefinition 2. consume http response for tcm submit to avoid warning

--- GGPClient-Android/src/com/openfeint/qa/ggp/step_definitions/People_StepDefinitions.java
+++ GGPClient-Android/src/com/openfeint/qa/ggp/step_definitions/People_StepDefinitions.java
@@ -5,9 +5,10 @@ import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertTrue;
 import static junit.framework.Assert.fail;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import com.openfeint.qa.core.caze.step.definition.BasicStepDefinition;
+import com.openfeint.qa.core.command.Given;
+import com.openfeint.qa.core.command.Then;
+import com.openfeint.qa.core.command.When;
 
 import net.gree.asdk.api.GreePlatform;
 import net.gree.asdk.api.GreeUser;
@@ -16,12 +17,11 @@ import net.gree.asdk.api.GreeUser.GreeUserListener;
 import org.apache.http.HeaderIterator;
 
 import util.Consts;
+
 import android.util.Log;
 
-import com.openfeint.qa.core.caze.step.definition.BasicStepDefinition;
-import com.openfeint.qa.core.command.Given;
-import com.openfeint.qa.core.command.Then;
-import com.openfeint.qa.core.command.When;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class People_StepDefinitions extends BasicStepDefinition {
     private static final String TAG = "People_Steps";
@@ -32,48 +32,10 @@ public class People_StepDefinitions extends BasicStepDefinition {
 
     private final static String FRIEND = "friend";
 
-    private String status;
-
-    // waiting for the async callback and assert it success
-    private void waitCallback() {
-        while (true) {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            if (status != Consts.UNKNOWN) {
-                assertEquals("server response", Consts.SUCCESS, status);
-                break;
-            }
-        }
-    }
-
-    GreeUserListener listener = new GreeUserListener() {
-        @Override
-        public void onSuccess(int index, int count, GreeUser[] people) {
-            Log.d(TAG, "Get people success!");
-            getBlockRepo().put(PEOPLE_LIST, new ArrayList<GreeUser>());
-            if (people != null) {
-                Log.i(TAG, "Get " + people.length + " people");
-                Log.i(TAG, "Adding people datas");
-                ((ArrayList<GreeUser>)getBlockRepo().get(PEOPLE_LIST)).addAll(Arrays.asList(people));
-            }
-            status = Consts.SUCCESS;
-        }
-
-        @Override
-        public void onFailure(int responseCode, HeaderIterator headers, String response) {
-            getBlockRepo().put(PEOPLE_LIST, new ArrayList<GreeUser>());
-            Log.e(TAG, "Get people failed!");
-            status = Consts.FAILED;
-        }
-    };
-
     @Given("I logged in with email (.+) and password (\\w+)")
     public void checkLogin(String email, String password) {
         // nothing to do in this method
+        notifyNext();
     }
 
     @When("I see my info from native cache")
@@ -84,6 +46,7 @@ public class People_StepDefinitions extends BasicStepDefinition {
             getBlockRepo().put(MYSELF, GreePlatform.getLocalUser());
             Log.i(TAG, "Logined as user: " + GreePlatform.getLocalUser().getNickname());
         }
+        notifyNext();
     }
 
     @Then("my (\\w+) should be (.+)")
@@ -101,41 +64,57 @@ public class People_StepDefinitions extends BasicStepDefinition {
         } else {
             fail("Unknown column of user info!");
         }
+        notifyNext();
     }
 
     @When("I see my info from server")
     public void getUserInfoFromServer() {
-        status = Consts.UNKNOWN;
-        GreeUser.loadUserWithId(1, 1, "@me", listener);
-        waitCallback();
-        ArrayList<GreeUser> l = (ArrayList<GreeUser>) getBlockRepo().get(PEOPLE_LIST);
-        if (l == null)
-            fail("No user info got!");
+        GreeUser.loadUserWithId(1, 1, "@me", new GreeUserListener() {
+            @Override
+            public void onSuccess(int index, int count, GreeUser[] people) {
+                Log.d(TAG, "Get people success!");
+                getBlockRepo().put(PEOPLE_LIST, new ArrayList<GreeUser>());
+                if (people != null) {
+                    Log.i(TAG, "Get " + people.length + " people");
+                    Log.i(TAG, "Adding people datas");
+                    getBlockRepo().put(MYSELF, people[0]);
+                    Log.i(TAG, "User info get from server: " + people[0]);
+                }
+                notifyNext();
+            }
 
-        getBlockRepo().put(MYSELF, l.get(0));
-        Log.i(TAG, "User info get from server: " + l.get(0).getNickname());
-    }
-
-    private void getFriends(GreeUser owner) {
-        status = Consts.UNKNOWN;
-        owner.loadFriends(Consts.startIndex_1, Consts.pageSize, listener);
-        waitCallback();
-        ArrayList<GreeUser> l = (ArrayList<GreeUser>) getBlockRepo().get(PEOPLE_LIST);
-        if (l == null)
-            fail("No friend got!");
-
-        for (GreeUser person : l) {
-            Log.i(TAG, "Friend " + (l.indexOf(person) + 1));
-            Log.i(TAG, "ID: " + person.getId());
-            Log.i(TAG, "Nickname: " + person.getNickname());
-            Log.i(TAG, "UserGrade: " + person.getUserGrade());
-        }
+            @Override
+            public void onFailure(int responseCode, HeaderIterator headers, String response) {
+                Log.e(TAG, "Get people failed!");
+                notifyNext();
+            }
+        });
     }
 
     @When("I check my friend list")
     public void getCurrentUserFriends() {
         GreeUser me = GreePlatform.getLocalUser();
-        getFriends(me);
+        me.loadFriends(Consts.startIndex_1, Consts.pageSize, new GreeUserListener() {
+            @Override
+            public void onSuccess(int index, int count, GreeUser[] people) {
+                Log.d(TAG, "Get people success!");
+                getBlockRepo().put(PEOPLE_LIST, new ArrayList<GreeUser>());
+                if (people != null) {
+                    Log.i(TAG, "Get " + people.length + " people");
+                    Log.i(TAG, "Adding people datas");
+                    ((ArrayList<GreeUser>) getBlockRepo().get(PEOPLE_LIST)).addAll(Arrays
+                            .asList(people));
+                }
+                notifyNext();
+            }
+
+            @Override
+            public void onFailure(int responseCode, HeaderIterator headers, String response) {
+                getBlockRepo().put(PEOPLE_LIST, new ArrayList<GreeUser>());
+                Log.e(TAG, "Get people failed!");
+                notifyNext();
+            }
+        });
     }
 
     @Then("friend list should be size of (\\d+)")
@@ -143,6 +122,7 @@ public class People_StepDefinitions extends BasicStepDefinition {
         Log.d(TAG, "Checking friend number...");
         ArrayList<GreeUser> l = (ArrayList<GreeUser>) getBlockRepo().get(PEOPLE_LIST);
         assertEquals("friend count", num, l.size());
+        notifyNext();
     }
 
     @Then("friend list should have (.+)")
@@ -155,35 +135,51 @@ public class People_StepDefinitions extends BasicStepDefinition {
                 Log.d(TAG, "Got the user");
                 assertTrue("user in friend list", true);
                 getBlockRepo().put(FRIEND, user);
+                notifyNext();
                 return;
             }
         }
         assertTrue("user " + friendName + " in friend list", false);
+        notifyNext();
     }
 
     @Then("userid of (.+) should be (\\w+) and grade should be (\\w+)")
     public void verifyFriendInfo(String name, String id, String grade) {
         GreeUser friend = (GreeUser) getBlockRepo().get(FRIEND);
+
         if (friend == null)
             fail("Don't get the friend " + name + " in the list");
 
         Log.d(TAG, "Checking friend info...");
         assertEquals("userId", id, friend.getId());
         assertEquals("userGrade", grade, friend.getUserGrade());
+        notifyNext();
     }
 
     // This step is not using now, seems the sdk is not allow to get another
     // user instance except the current login user
     @When("I check friend list of user (\\w+)")
     public void getSpecificUserFriends(String userId) {
-        status = Consts.UNKNOWN;
-        GreeUser.loadUserWithId(1, 1, userId, listener);
-        waitCallback();
-        ArrayList<GreeUser> l = (ArrayList<GreeUser>) getBlockRepo().get(PEOPLE_LIST);
-        if (l == null)
-            fail("Don't get the friend in the list");
+        GreeUser.loadUserWithId(1, 1, userId, new GreeUserListener() {
+            @Override
+            public void onSuccess(int index, int count, GreeUser[] people) {
+                Log.d(TAG, "Get people success!");
+                getBlockRepo().put(PEOPLE_LIST, new ArrayList<GreeUser>());
+                if (people != null) {
+                    Log.i(TAG, "Get " + people.length + " people");
+                    Log.i(TAG, "Adding people datas");
+                    ((ArrayList<GreeUser>) getBlockRepo().get(PEOPLE_LIST)).addAll(Arrays
+                            .asList(people));
+                }
+                notifyNext();
+            }
 
-        Log.d(TAG, "specific user is: " + l.get(0).getNickname());
-        getFriends(l.get(0));
+            @Override
+            public void onFailure(int responseCode, HeaderIterator headers, String response) {
+                getBlockRepo().put(PEOPLE_LIST, new ArrayList<GreeUser>());
+                Log.e(TAG, "Get people failed!");
+                notifyNext();
+            }
+        });
     }
 }
--- OFQAAPI/src/com/openfeint/qa/core/caze/step/definition/BasicStepDefinition.java
+++ OFQAAPI/src/com/openfeint/qa/core/caze/step/definition/BasicStepDefinition.java
@@ -2,14 +2,19 @@
 package com.openfeint.qa.core.caze.step.definition;
 
 import java.util.Hashtable;
+import java.util.Observable;
 
-public abstract class BasicStepDefinition {
+public abstract class BasicStepDefinition extends Observable{
     protected static Hashtable<String, Object> blockRepo = null;
-    
-    protected static Hashtable<String, Object> getBlockRepo(){
-        if(null == blockRepo){
+    protected static Hashtable<String, Object> getBlockRepo() {
+        if (null == blockRepo) {
             blockRepo = new Hashtable<String, Object>();
         }
         return blockRepo;
+    }
+
+    protected void notifyNext() {
+        setChanged();
+        notifyObservers("update waiting");
     }
 }
