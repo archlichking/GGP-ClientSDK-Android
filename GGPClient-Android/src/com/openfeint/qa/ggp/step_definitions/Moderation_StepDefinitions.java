9d5cef93e8cd95b960778f012062c494f485b27b
Coco Matthey <coco.matthey@aurorafeint.com>
2012-05-01 23:00:13 -0700

    one step at a time

--- GGPClient-Android/src/com/openfeint/qa/ggp/step_definitions/Moderation_StepDefinitions.java
+++ GGPClient-Android/src/com/openfeint/qa/ggp/step_definitions/Moderation_StepDefinitions.java
@@ -1,4 +1,3 @@
-
 package com.openfeint.qa.ggp.step_definitions;
 
 import static junit.framework.Assert.assertEquals;
@@ -6,12 +5,16 @@ import static junit.framework.Assert.assertTrue;
 import static junit.framework.Assert.fail;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Vector;
 
 import net.gree.asdk.api.GreePlatform;
 import net.gree.asdk.api.GreeUser;
 import net.gree.asdk.api.GreeUser.GreeUserListener;
-import net.gree.asdk.api.Moderation;
+import net.gree.asdk.api.ModeratedText;
+import net.gree.asdk.api.ModeratedText.ModeratedTextListener;
+import net.gree.asdk.api.ModeratedText.SuccessListener;
 import net.gree.asdk.core.GLog;
 
 import org.apache.http.HeaderIterator;
@@ -25,59 +28,31 @@ import com.openfeint.qa.core.command.Then;
 import com.openfeint.qa.core.command.When;
 
 public class Moderation_StepDefinitions extends BasicStepDefinition {
-    private static final String TAG = "Moderation_Steps";
+  private static final String TAG = "Moderation_Steps";
 
-    private static List<Moderation.ResponseArray> moderationList = new ArrayList<Moderation.ResponseArray>();
+  private static String MODERATION_LIST = "moderationlist";
 
-    private String status;
-
-    // waiting for the async callback and assert it success
-    private void waitCallback() {
-        while (true) {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            if (status != Consts.UNKNOWN) {
-                assertEquals("server response", Consts.SUCCESS, status);
-                break;
-            }
-        }
-    }
-    
-    Moderation.TextArrayListener moderationListener = new Moderation.TextArrayListener() {
+  @When("I send to moderation server with text (.*)")
+  public void sendModeration(String text) {
+    ModeratedText.create(text, new ModeratedTextListener() {
       @Override
-      public void onSuccess(int responseCode, HeaderIterator headers,
-          Moderation.ResponseArray textInfo) {
-        String id = textInfo.entry[0].textId;
-        String statusStr = textInfo.entry[0].status;
-        int statusi = 0;
-        try {
-          statusi = Integer.parseInt(statusStr);
-        } catch (Exception ex) {}
-        Moderation.logTextInfo(textInfo.entry);
-        status = Consts.SUCCESS;
+      public void onSuccess(ModeratedText[] textInfo) {
+        Log.d(TAG, "onSuccess");
+        getBlockRepo().put(MODERATION_LIST, new ArrayList<ModeratedText>());
+        ModeratedText.logTextInfo(textInfo);
+
+        ((ArrayList<ModeratedText>) getBlockRepo().get(MODERATION_LIST)).addAll(Arrays
+            .asList(textInfo));
+        notifyStepPass();
       }
+
       @Override
       public void onFailure(int responseCode, HeaderIterator headers, String response) {
-        Log.e(TAG, "Get moderation failed!");
-        moderationList.clear();
-        status = Consts.FAILED;
+        Log.e(TAG, "Get moderated text failed!");
+        getBlockRepo().put(MODERATION_LIST, new ArrayList<ModeratedText>());
+        notifyStepPass();
       }
-    };
-    
-    @When("I send to moderation server with text (.*)")
-    public void sendModeration(String text) {
-      Moderation moderation = new Moderation();
-      moderation.create(text, moderationListener);
-      waitCallback(); // add here?
-    }
-    
-    @Then("Then status of text (.*) in server should be CHECKED")
-    public void checkStatusText() {
-      //TODO
-    }
+    });
+  }
 
 }
