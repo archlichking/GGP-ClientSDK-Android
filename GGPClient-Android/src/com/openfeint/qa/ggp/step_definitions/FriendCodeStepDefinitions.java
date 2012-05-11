c8cd3aa5581ca9090a1c4d3c73dac6fb3c91e65b
Vincent Xie [Xie WeiShen] <vincent.xie@gree.co.jp>
2012-05-11 16:20:56 +0800

    fix the change in step title

--- GGPClient-Android/src/com/openfeint/qa/ggp/step_definitions/FriendCodeStepDefinitions.java
+++ GGPClient-Android/src/com/openfeint/qa/ggp/step_definitions/FriendCodeStepDefinitions.java
@@ -52,12 +52,18 @@ public class FriendCodeStepDefinitions extends BasicStepDefinition {
             fail("Unknown isExistMark!");
     }
 
+    @When("I request friend code with no expire time")
+    public void requestNoExpireTimeCode() {
+        requestFriendCode("");
+    }
+
     @When("I request friend code with expire time (.+)")
     public void requestFriendCode(String expireTime) {
         CodeListener listener = new CodeListener() {
             @Override
             public void onSuccess(Code code) {
                 Log.d(TAG, "Add new friend code success: " + code.getCode());
+                Log.d(TAG, "Expire time is: " + code.getExpireTime());
                 getBlockRepo().put(FRIEND_CODE, code);
                 notifyStepPass();
             }
@@ -71,11 +77,7 @@ public class FriendCodeStepDefinitions extends BasicStepDefinition {
 
         getBlockRepo().put(FRIEND_CODE, "");
         notifyStepWait();
-        if ("NONE".equals(expireTime)) {
-            FriendCode.requestCode("", listener);
-        } else {
-            FriendCode.requestCode(expireTime, listener);
-        }
+        FriendCode.requestCode(expireTime, listener);
     }
 
     @Then("I should get my friend code")
@@ -89,14 +91,14 @@ public class FriendCodeStepDefinitions extends BasicStepDefinition {
         String expireTime = ((Code) getBlockRepo().get(FRIEND_CODE)).getExpireTime();
         if (expectTime.endsWith("days")) {
             int length = Integer.parseInt(expectTime.split(" ")[0]);
-            
+
             // Calculate the expire date
             SimpleDateFormat formater = new SimpleDateFormat("yyyy-MM-dd");
             Calendar cal = Calendar.getInstance();
             cal.add(Calendar.DAY_OF_MONTH, length);
             cal.add(Calendar.HOUR, -8); // reduce 8 hours to UTC time zone
             String expireDate = formater.format(cal.getTime());
-            
+
             Log.d(TAG, "Checking the expire date...");
             assertEquals("Expire date", expireDate, expireTime.substring(0, 10));
         } else {
