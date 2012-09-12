119fac51178ea2de6aee37116f1eecd77e5a5ee2
Coco Matthey <coco.matthey@aurorafeint.com>
2012-09-11 17:13:01 -0700

    GIIP-170|GIIP-106 [Android] add tests on DeviceInfo.java GENERAL COMMENT ON THIS COMMIT

--- GGPClient-Android/src/com/openfeint/qa/ggp/step_definitions/DeviceInfoStepDefinitions.java
+++ GGPClient-Android/src/com/openfeint/qa/ggp/step_definitions/DeviceInfoStepDefinitions.java
@@ -1,20 +1,13 @@
 
 package com.openfeint.qa.ggp.step_definitions;
 
-import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertTrue;
 import static junit.framework.Assert.fail;
-
-import java.util.HashMap;
-import java.util.Map;
+import net.gree.asdk.core.request.OnResponseCallback;
+import net.gree.asdk.core.util.DeviceInfo;
 
 import org.apache.http.HeaderIterator;
 
-import net.gree.asdk.api.alarm.ScheduledNotification;
-import net.gree.asdk.api.alarm.ScheduledNotificationListener;
-import net.gree.asdk.core.request.OnResponseCallback;
-import net.gree.asdk.core.util.DeviceInfo;
-import android.bluetooth.BluetoothClass.Device;
 import android.util.Log;
 
 import com.openfeint.qa.core.caze.step.definition.BasicStepDefinition;
@@ -25,42 +18,18 @@ import com.openfeint.qa.core.command.When;
 
 public class DeviceInfoStepDefinitions extends BasicStepDefinition {
     private static final String TAG = DeviceInfoStepDefinitions.class.getSimpleName();
-    private static final String UDID = "udid";
-    private static final String MAC_ADDRESS = "mac_address";
-    private static final String UUID = "uuid";
-    private static final String IS_ROOTED = "is_rooted";
-    private static final String IS_SENDABLEANDROIDID = "is_sendableandroidid";
-    private static final String IS_SENDABLEMACADDRESS = "is_sendablemacaddress";
-    
     
     @When("I get the DeviceInfo")
     @And("I get the DeviceInfo")
     public void getDeviceInfo() {
     }
-//    @When("I get the device udid")
-//    @And("I get the device udid")
-//    public void getUdid() {
-//    	String udid = DeviceInfo.getUdid();
-//    	Log.d(TAG, udid);
-//    	getBlockRepo().put(UDID, udid);
-//    }
     
     @Then("the device udid should starts with android-id-")
     @And("the device udid should starts with android-id-")
     public void verifyUdid() {
-//    	String udid;
-//    	udid = (String) getBlockRepo().get(UDID);
     	Log.d(TAG, DeviceInfo.getUdid());
     	assertTrue("expected udid starts with android-id- but got " + DeviceInfo.getUdid(), DeviceInfo.getUdid().startsWith("android-id-") || DeviceInfo.getUdid().startsWith("android-emu-"));
     }
-    
-//    @When("I get the device mac address")
-//    @And("I get the device mac address")
-//    public void getMacAddress() {
-//    	String mac_address = DeviceInfo.getMacAddress();
-//    	Log.d(TAG, mac_address);
-//    	getBlockRepo().put(MAC_ADDRESS, mac_address);
-//    }
     
     @Then("the device mac address should be a well formed mac address")
     @And("the device mac address should be a well formed mac address")
@@ -91,72 +60,32 @@ public class DeviceInfoStepDefinitions extends BasicStepDefinition {
 			}
 		});
     }
-
-//    @When("I get the device uuid")
-//    @And("I get the device uuid")
-//    public void getUuid() {
-//    	String uuid = DeviceInfo.getUuid();
-//    	Log.d("uuid", (null == uuid) ? "null" : uuid);
-//    	getBlockRepo().put(UUID, uuid);
-//    }
     
     @Then("the device uuid should starts with uuid-")
     @And("the device uuid should starts with uuid-")
     public void verifyUuid() {
-//    	String uuid;
-//    	uuid = (String) getBlockRepo().get(UUID);
     	Log.d(TAG, (null == DeviceInfo.getUuid()) ? "null" : DeviceInfo.getUuid());
     	assertTrue("expected uuid starts with uuid- but got " + DeviceInfo.getUuid(), DeviceInfo.getUuid().startsWith("uuid-"));
     }
     
-//    @When("I check the device is rooted")
-//    @And("I check the device is rooted")
-//    public void getIsRooted() {
-//    	boolean isRooted = DeviceInfo.isRooted();
-//    	Log.d(TAG, String.valueOf(isRooted));
-//    	getBlockRepo().put(IS_ROOTED, isRooted);
-//    }
-//    
-//    @When("I check the device is SendableAndroidId")
-//    @And("I check the device is SendableAndroidId")
-//    public void getIsSendableAndroidId() {
-//    	boolean isSendableAndroidId = DeviceInfo.isSendableAndroidId();
-//    	Log.d(TAG, String.valueOf(isSendableAndroidId));
-//    	getBlockRepo().put(IS_SENDABLEANDROIDID, isSendableAndroidId);
-//    }
-//    
-//    @When("I check the device is SendableMacAddress")
-//    @And("I check the device is SendableMacAddress")
-//    public void getIsSendableMacAddress() {
-//    	boolean isSendableMacAddress = DeviceInfo.isSendableMacAddress();
-//    	Log.d(TAG, String.valueOf(isSendableMacAddress));
-//    	getBlockRepo().put(IS_SENDABLEMACADDRESS, isSendableMacAddress);
-//    }
-    
     @Then("the device is rooted should be (\\w+)")
     @And("the device is rooted should be (\\w+)")
-    public void VerifyIsRooted(String shouldBeRooted) {
-    	boolean shouldBeRootedFlag = Boolean.valueOf(shouldBeRooted);
-//    	boolean isRooted = (Boolean) getBlockRepo().get(IS_ROOTED);
+    public void VerifyIsRooted(boolean shouldBeRooted) {
     	Log.d(TAG, String.valueOf(DeviceInfo.isRooted()));
-    	assertTrue("isRooted should be " + shouldBeRooted, shouldBeRootedFlag == DeviceInfo.isRooted());
+    	assertTrue("isRooted should be " + shouldBeRooted, shouldBeRooted == DeviceInfo.isRooted());
     }
     
     @Then("the device isSendableAndroidId should be (\\w+)")
     @And("the device isSendableAndroidId should be (\\w+)")
-    public void VerifyIsSendableAndroidId(String shouldBeSendableAndroidId) {
-    	boolean shouldBeSendableAndroidIdFlag = Boolean.valueOf(shouldBeSendableAndroidId);
-//    	boolean isSendableAndroidId = (Boolean) getBlockRepo().get(IS_SENDABLEANDROIDID);
+    public void VerifyIsSendableAndroidId(boolean shouldBeSendableAndroidId) {
     	Log.d(TAG, String.valueOf(DeviceInfo.isSendableAndroidId()));
-    	assertTrue("isSendableAndroidId should be " + shouldBeSendableAndroidId, shouldBeSendableAndroidIdFlag == DeviceInfo.isSendableAndroidId());
+    	assertTrue("isSendableAndroidId should be " + shouldBeSendableAndroidId, shouldBeSendableAndroidId == DeviceInfo.isSendableAndroidId());
     }
     
     @Then("the device isSendableMacAddress should be (\\w+)")
     @And("the device isSendableMacAddress should be (\\w+)")
-    public void VerifyIsSendableMacAddress(String shouldBeSendableMacAddress) {
-    	boolean shouldBeSendableMacAddressFlag = Boolean.valueOf(shouldBeSendableMacAddress);
-//    	boolean isSendableMacAddress = (Boolean) getBlockRepo().get(IS_SENDABLEMACADDRESS);
+    public void VerifyIsSendableMacAddress(boolean shouldBeSendableMacAddress) {
     	Log.d(TAG, String.valueOf(DeviceInfo.isSendableMacAddress()));
-    	assertTrue("isSendableAndroidId should be " + shouldBeSendableMacAddress, shouldBeSendableMacAddressFlag == DeviceInfo.isSendableMacAddress());
+    	assertTrue("isSendableAndroidId should be " + shouldBeSendableMacAddress, shouldBeSendableMacAddress == DeviceInfo.isSendableMacAddress());
     }
 }
