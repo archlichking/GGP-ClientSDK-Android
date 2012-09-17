4b81d913e567bf711928221b272122257437e343
Vincent Xie [Xie WeiShen] <vincent.xie@gree.co.jp>
2012-09-17 17:12:34 +0800

    update ant target from 10 to 16

--- auto-script/BuildAutoApp.sh
+++ auto-script/BuildAutoApp.sh
@@ -22,34 +22,7 @@ apk_path="bin/GGPClient-Automation-release.apk"
 
 # ========== Action Begin ==========
 
-##Copy local.properties to all relied project
-#
-#if [ ! -f $local_pro ] || [ ! -f $build_xml ]
-#then
-#  echo "$local_pro or $build_xml is not exists, generate a new one"
-#  sh $script_root"GenConfigFiles.sh"
-#fi
-#
-##Copy local.properties
-#cp $local_pro $project_root
-#cp $local_pro $project_root"../OFQAAPI/"
-#cp $local_pro $sdk_root$sdk_path
-#cp $local_pro $sdk_root$gson_path
-#cp $local_pro $sdk_root$PTR_path
-#cp $local_pro $sdk_root$signpost_core_path
-#cp $local_pro $sdk_root$signpost_common_path
-#
-##Copy build.xml
-#flag="true"
-#if [ "true" == $flag ]
-#then
-#  cp $build_xml $sdk_root$sdk_path
-#  cp $build_xml $sdk_root$gson_path
-#  cp $build_xml $sdk_root$PTR_path
-#  cp $build_xml $sdk_root$signpost_core_path
-#  cp $build_xml $sdk_root$signpost_common_path
-#fi
-
+# Update build.xml and local.properties for all sub-projects
 cd $workspace_root$project_root
 android update project -p .
 cd $workspace_root$qalib_root
@@ -65,7 +38,7 @@ android update project -p .
 cd $workspace_root$sdk_root$signpost_common_path
 android update project -p .
 
-#Use ant to build the project
+# Use ant to build the project
 cd $workspace_root$project_root
 ant clean > /dev/null
 ant release >/dev/null
