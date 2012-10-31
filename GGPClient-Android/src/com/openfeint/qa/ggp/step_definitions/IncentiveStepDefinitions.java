7e874133eb52638ab797cbf0176160c704913f24
Coco Matthey <coco.matthey@aurorafeint.com>
2012-10-30 17:23:59 -0700

    fix after the comments

--- GGPClient-Android/src/com/openfeint/qa/ggp/step_definitions/IncentiveStepDefinitions.java
+++ GGPClient-Android/src/com/openfeint/qa/ggp/step_definitions/IncentiveStepDefinitions.java
@@ -1,9 +1,10 @@
 package com.openfeint.qa.ggp.step_definitions;
 
 import static junit.framework.Assert.fail;
-
+import static junit.framework.Assert.assertEquals;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import net.gree.asdk.api.incentive.IncentiveController;
@@ -27,6 +28,7 @@ import com.openfeint.qa.core.command.After;
 import com.openfeint.qa.core.command.And;
 import com.openfeint.qa.core.command.Then;
 import com.openfeint.qa.core.command.When;
+import com.openfeint.qa.core.util.CredentialStorage;
 import com.openfeint.qa.ggp.MainActivity;
 import com.openfeint.qa.ggp.R;
 
@@ -35,12 +37,13 @@ public class IncentiveStepDefinitions extends BasicStepDefinition {
 			.getSimpleName();
 	private static final String INCENTIVE_PAYLOAD = "incentive_payload";
 	private static final String PAYLOAD_TYPE = "payload_type";
-	private static final String NUM_OF_INSERTED_ROWS = "num_of_inserted_rows";
+	private static final String ENTRIES = "entries";
 	private static final String INCENTIVE_EVENTS = "incentive_events";
 	private static final String EVENT_IDS = "event_ids";
 
 	@And("I initialize incentive with incentive type (.*) and message (.*)")
 	public void initializeIncentive(String type, String message) {
+		
 		int payloadType;
 		if ("request".equals(type)) {
 			payloadType = IncentiveController.PAYLOAD_TYPE_REQUEST;
@@ -55,17 +58,24 @@ public class IncentiveStepDefinitions extends BasicStepDefinition {
 		JsonObject jsonObject = new JsonObject();
 		jsonObject.addProperty("message", message);
 		String json = jsonObject.toString();
+		
 		getBlockRepo().put(PAYLOAD_TYPE, payloadType);
 		getBlockRepo().put(INCENTIVE_PAYLOAD, json);
 	}
 
 	@When("I send incentive to user of email (.*)")
-	public void postIncentive(String email) {
-		notifyStepWait();
+	public void postIncentive(String concatenatedEmails) {
+		String[] emails = concatenatedEmails.split(",");
 		List<String> users = new ArrayList<String>();
-		String userId = emailToUserId(email);
-		Log.d(TAG, "send incentive to " + email + " whose user id is " + userId);
-		users.add(userId);
+		CredentialStorage credentialStorage = CredentialStorage.getInstance();
+		HashMap<String, String> pair = credentialStorage.getEmailIdPairs();
+		for (String email : emails) {
+			String userId = pair.get(email);			
+			users.add(userId);
+			Log.d(TAG, "send incentive to " + email + " whose user id is " + userId);
+		}
+		getBlockRepo().remove(ENTRIES);
+		notifyStepWait();
 		IncentiveController.post(users,
 				((Integer) getBlockRepo().get(PAYLOAD_TYPE)).intValue(),
 				(String) getBlockRepo().get(INCENTIVE_PAYLOAD),
@@ -76,7 +86,7 @@ public class IncentiveStepDefinitions extends BasicStepDefinition {
 						Log.d(TAG, "send incentive success");
 						try {
 							int number = parseNumberOfInsertedRows(response);
-							getBlockRepo().put(NUM_OF_INSERTED_ROWS, number);
+							getBlockRepo().put(ENTRIES, number);
 						} catch (JSONException e) {
 							fail("The server does not return the number of inserted rows: "
 									+ response);
@@ -94,43 +104,6 @@ public class IncentiveStepDefinitions extends BasicStepDefinition {
 				});
 	}
 
-	private String emailToUserId(String email) {
-		// see MainActivity.java
-		RawFileUtil rfu = RawFileUtil.getInstance(MainActivity.getInstance());
-		String app_id = "15265"; // on sandbox or production
-		String field_name = "credentials_config_" + app_id;
-		field_name = "credentials_config_15265";
-		int resource_id = 0;
-		try {
-			Field field = R.raw.class.getDeclaredField(field_name);
-			resource_id = (Integer) field.get(null);
-			Log.d(TAG, String.valueOf(resource_id));
-		} catch (SecurityException e) {
-			e.printStackTrace();
-		} catch (NoSuchFieldException e) {
-			e.printStackTrace();
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		}
-		try {
-			String data = rfu.getTextFromRawResource(resource_id);
-			org.json.JSONObject jSONObject = new org.json.JSONObject(data);
-			JSONArray jsonArray = jSONObject.getJSONArray("credentials");
-			for (int i = 0; i < jsonArray.length(); i++) {
-				org.json.JSONObject jobj = jsonArray.getJSONObject(i);
-				String userid = jobj.getString("userid");
-				String username = jobj.getString("username");
-				if (username.equals(email))
-					return userid;
-			}
-		} catch (JSONException e) {
-			return null;
-		}
-		return null;
-	}
-
 	private int parseNumberOfInsertedRows(String json) throws JSONException {
 		org.json.JSONObject jsonObject = new JSONObject(json);
 		int number = jsonObject.getInt("entry");
@@ -139,16 +112,13 @@ public class IncentiveStepDefinitions extends BasicStepDefinition {
 
 	@Then("incentive target should be (.*)")
 	public void verifyIncentive(int number) {
-		int number_reported_by_server = (Integer) getBlockRepo().get(
-				NUM_OF_INSERTED_ROWS);
-		if (number_reported_by_server != number) {
-			Log.d(TAG, "server return is success");
-			fail("expected " + number + " inserted rows but only reported "
-					+ number_reported_by_server + " by the server");
-		}
+		int entries = (Integer) getBlockRepo().get(
+				ENTRIES);
+		assertEquals("server returns "+ number, number, entries);
 	}
 
 	@After("I mark incentive as processed")
+	@And("I mark incentive as processed")
 	public void processIncentive() {
 		notifyStepWait();
 		getIncentive();
@@ -172,9 +142,11 @@ public class IncentiveStepDefinitions extends BasicStepDefinition {
 	}
 
 	private void getIncentive() {
-		notifyStepWait();
-		IncentiveController.get(false, null, new IncentiveGetListener() {
-
+		getBlockRepo().remove(INCENTIVE_EVENTS);
+		HashMap<String, String> params = new HashMap<String, String>();
+//		params.put(IncentiveController.KEY_COUNT, "100");
+		IncentiveController.get(false, params, new IncentiveGetListener() {
+			
 			@Override
 			public void onSuccess(IncentiveEventArray result) {
 				int totalResult = result.getTotalResults();
@@ -189,15 +161,16 @@ public class IncentiveStepDefinitions extends BasicStepDefinition {
 					ids.add(incentiveEvents[i].getId());
 				}
 				getBlockRepo().put(EVENT_IDS, ids);
-				notifyStepPass();
+				notifyAsyncInStep();
 			}
 
 			@Override
 			public void onFailure(int responseCode, HeaderIterator headers,
 					String response) {
 				fail("failed to get incentive events");
-				notifyStepPass();
+				notifyAsyncInStep();
 			}
 		});
+		waitForAsyncInStep();
 	}
 }
